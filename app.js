ba8de750d70ce69afa37abe1bfa34dcf04ccb665
francieli-alvarenga <francielereis0@gmail.com>
2023-03-23 20:45:40 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("rodando o sistema de gerenciamento de jogos ")
 console.log("aplicacao esta se conectando ao bando de dados")
 console.log("enviando para o github")
+console.log("modificacao")
